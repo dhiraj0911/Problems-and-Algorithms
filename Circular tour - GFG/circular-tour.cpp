0f102a33a030239ad813d2dcbc22b4de32ea4be1
Dhiraj <82199369+dhiraj0911@users.noreply.github.com>
2022-03-03 17:56:58 +0530

    Added solution - LeetHub

---
+++ Circular tour - GFG/circular-tour.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+ // } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    int tour(petrolPump p[],int n)
+    {
+        int pet = 0, index = 0, dist = 0;
+        for(int i = 0; i<n; i++){
+            pet += p[i].petrol;
+            if(pet<p[i].distance){
+                dist += p[i].distance - pet;
+                pet = 0;
+                index = (i+1)%n;
+            }
+            else{
+                pet -= p[i].distance;
+            }
+        }
+        if(pet-dist < 0)
+            return -1;
+        return index;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+  // } Driver Code Ends
