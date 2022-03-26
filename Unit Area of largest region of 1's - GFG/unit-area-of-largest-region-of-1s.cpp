4835ef9bae980706355abbed2af81195c51c3246
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-03-26 18:04:13 +0530

    Added solution - LeetHub

--- Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
+++ Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
@@ -39,43 +39,6 @@ class Solution
         }
         return ans;
     }
-    
-//     void dfs(int i,int j,int row,int col,vector<vector<int>>& grid,vector<vector<bool>>&visited,int &length){
-//       if(i<0 || i>=row || j<0 || j>=col || visited[i][j]==true || grid[i][j]==0)
-//       return;
-       
-//       length++;
-//       visited[i][j]=true;
-       
-//                   dfs(i-1,j,row,col,grid,visited,length);
-//                   dfs(i+1,j,row,col,grid,visited,length); 
-//                   dfs(i,j+1,row,col,grid,visited,length);
-//                   dfs(i,j-1,row,col,grid,visited,length);
-//                   dfs(i+1,j+1,row,col,grid,visited,length);
-//                   dfs(i+1,j-1,row,col,grid,visited,length);
-//                   dfs(i-1,j-1,row,col,grid,visited,length);
-//                   dfs(i-1,j+1,row,col,grid,visited,length);
-       
-//   }
-//   //Function to find unit area of the largest region of 1s.
-//   int findMaxArea(vector<vector<int>>& grid) {
-//       // Code here
-//       int row=grid.size();
-//       int col=grid[0].size();
-//       vector<vector<bool>>visited(row,vector<bool>(col,false));
-//       int ans=0;
-//       int length=0;
-//       for(int i=0;i<row;i++){
-//           for(int j=0;j<col;j++){
-//               if(visited[i][j]==false && grid[i][j]==1){
-//                   dfs(i,j,row,col,grid,visited,length);
-//                   ans=max(ans,length);
-//                   length=0;
-//               }
-//           }
-//       }
-//       return ans;
-//   }
 };
 
 // { Driver Code Starts.
