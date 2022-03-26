1ca0c4c8ec40d09bea274956c58ff6f4a2b527e5
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-03-26 18:04:03 +0530

    Added solution - LeetHub

--- Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
+++ Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
@@ -6,74 +6,76 @@ using namespace std;
 class Solution
 {
     public:
-    //Function to find unit area of the largest region of 1s.
-    // int length = 0;
-    // void counter(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited){
-    //     if(i < grid.size() || j < grid[0].size() || visited[i][j] == true){
-    //         return;
-    //     }
-    //     length++;
-    //     visited[i][j] = true;
-    //     counter(i, j+1, grid, visited);
-    //     counter(i, j-1, grid, visited);
-    //     counter(i+1, j, grid, visited);
-    //     counter(i-1, j, grid, visited);
-    //     counter(i+1, j+1, grid, visited);
-    //     counter(i+1, j-1, grid, visited);
-    //     counter(i-1, j+1, grid, visited);
-    //     counter(i-1, j-1, grid, visited);
-    // }
+    void counter(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited, int &length){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || visited[i][j] == true || grid[i][j] == 0){
+            return;
+        }
+        length++;
+        visited[i][j] = true;
+        counter(i, j+1, grid, visited, length);
+        counter(i, j-1, grid, visited, length);
+        counter(i+1, j, grid, visited, length);
+        counter(i-1, j, grid, visited, length);
+        counter(i+1, j+1, grid, visited, length);
+        counter(i+1, j-1, grid, visited, length);
+        counter(i-1, j+1, grid, visited, length);
+        counter(i-1, j-1, grid, visited, length);
+    }
     
-    // int findMaxArea(vector<vector<int>>& grid) {
-    //     int n = grid.size();
-    //     int m = grid[0].size();
-    //     vector<vector<bool>> visited(n, vector<bool> (m, false));
-    //     for(int i = 0; i < n; i++){
-    //         for(int j = 0; j < m; j++){
-    //             if(grid[i][j] == 1 && visited[i][j] == false){
-    //               counter(i, j, grid, visited);
-    //             }
-    //         }
-    //     }
-    //     return length;
-    // }
+    int findMaxArea(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        int length = 0;
+        vector<vector<bool>> visited(n, vector<bool> (m, false));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1 && visited[i][j] == false){
+                  counter(i, j, grid, visited, length);
+                  ans = max(ans, length);
+                  length = 0;
+                }
+            }
+        }
+        return ans;
+    }
     
-    void dfs(int i,int j,int row,int col,vector<vector<int>>& grid,vector<vector<bool>>&visited,int &length){
-       if(i<0 || i>=row || j<0 || j>=col || visited[i][j]==true || grid[i][j]==0)
-       return;
+//     void dfs(int i,int j,int row,int col,vector<vector<int>>& grid,vector<vector<bool>>&visited,int &length){
+//       if(i<0 || i>=row || j<0 || j>=col || visited[i][j]==true || grid[i][j]==0)
+//       return;
        
-       length++;
-       visited[i][j]=true;
+//       length++;
+//       visited[i][j]=true;
        
-                   dfs(i-1,j,row,col,grid,visited,length);
-                   dfs(i+1,j,row,col,grid,visited,length); 
-                   dfs(i,j+1,row,col,grid,visited,length);
-                   dfs(i,j-1,row,col,grid,visited,length);
-                   dfs(i+1,j+1,row,col,grid,visited,length);
-                   dfs(i+1,j-1,row,col,grid,visited,length);
-                   dfs(i-1,j-1,row,col,grid,visited,length);
-                   dfs(i-1,j+1,row,col,grid,visited,length);
+//                   dfs(i-1,j,row,col,grid,visited,length);
+//                   dfs(i+1,j,row,col,grid,visited,length); 
+//                   dfs(i,j+1,row,col,grid,visited,length);
+//                   dfs(i,j-1,row,col,grid,visited,length);
+//                   dfs(i+1,j+1,row,col,grid,visited,length);
+//                   dfs(i+1,j-1,row,col,grid,visited,length);
+//                   dfs(i-1,j-1,row,col,grid,visited,length);
+//                   dfs(i-1,j+1,row,col,grid,visited,length);
        
-   }
-   //Function to find unit area of the largest region of 1s.
-   int findMaxArea(vector<vector<int>>& grid) {
-       // Code here
-       int row=grid.size();
-       int col=grid[0].size();
-       vector<vector<bool>>visited(row,vector<bool>(col,false));
-       int ans=0;
-       int length=0;
-       for(int i=0;i<row;i++){
-           for(int j=0;j<col;j++){
-               if(visited[i][j]==false && grid[i][j]==1){
-                   dfs(i,j,row,col,grid,visited,length);
-                   ans=max(ans,length);
-                   length=0;
-               }
-           }
-       }
-       return ans;
-   }
+//   }
+//   //Function to find unit area of the largest region of 1s.
+//   int findMaxArea(vector<vector<int>>& grid) {
+//       // Code here
+//       int row=grid.size();
+//       int col=grid[0].size();
+//       vector<vector<bool>>visited(row,vector<bool>(col,false));
+//       int ans=0;
+//       int length=0;
+//       for(int i=0;i<row;i++){
+//           for(int j=0;j<col;j++){
+//               if(visited[i][j]==false && grid[i][j]==1){
+//                   dfs(i,j,row,col,grid,visited,length);
+//                   ans=max(ans,length);
+//                   length=0;
+//               }
+//           }
+//       }
+//       return ans;
+//   }
 };
 
 // { Driver Code Starts.
