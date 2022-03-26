8ad14c482fa86956727b178e620ae887da6c893d
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-03-26 16:24:41 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -63,66 +63,6 @@ class Solution
         }
         return count;
     }
-//     int orangesRotting(vector<vector<int>>& grid) {
-//         int cnt = 0;
-        
-//       int row = grid.size();
-//       int col = grid[0].size();
-       
-//       queue<pair<int,int>> q;
-       
-//       for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//       if(grid[i][j]==2){
-//         q.push({i,j});
-//       }
-//       }
-//       }
-       
-//       q.push({-1,-1});  
-       
-//       while(q.size()!=0){
-//         int a = q.front().first;
-//         int b = q.front().second;
-//         if(a!=-1 and b!=-1){
-//          if( b+1 <= col-1  && grid[a][b+1]==1) {
-//           grid[a][b+1] = 2;
-//           q.push({a,b+1});
-//          }
-//          if(a+1 <= row-1 && grid[a+1][b]==1){
-//             grid[a+1][b]=2;
-//             q.push({a+1,b});
-//          }
-//          if(b-1>=0 and grid[a][b-1]==1){
-//             grid[a][b-1]=2;
-//             q.push({a,b-1});
-//          }
-//          if(a-1>=0 and grid[a-1][b]==1){
-//             grid[a-1][b]=2;
-//             q.push({a-1,b});
-//          }
-//          q.pop();
-//         } else if (a==-1 and b==-1 and q.size() == 1){
-//           q.pop();
-//           break;
-//         } else if (a==-1 and b==-1 and q.size() > 1){
-//           q.pop();
-//           q.push({-1,-1});
-//           cnt++;
-//         }
-//       }
-       
-//       for(int i=0;i<row;i++){
-//       for(int j=0;j<col;j++){
-//       if(grid[i][j]==1){
-//         return -1;
-//       }
-//       }
-//       }
-       
-//       return cnt;
-       
-//   }
 };
 
 // { Driver Code Starts.
