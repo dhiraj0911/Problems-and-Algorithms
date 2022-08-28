729eb21883289b44638d66d1180b001e1e461770
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-08-28 14:17:42 +0530

    Time: 4 ms (45.23%), Space: 8.4 MB (23.94%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // map<int, int> m;
+        // for(int i = 0; i < nums.size(); i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto x: m){
+        //     if(x == 0)
+        // }
+        sort(nums.begin(), nums.end());
+    }
+};
