f17db17c177405a06ebb5ab97f2099a4a4491749
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-09-14 02:46:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
+        // if(n <= 1)
+        //     return n;
+        // return fib(n - 1) + fib(n - 2);
         if(n <= 1)
             return n;
-        return fib(n - 1) + fib(n - 2);
+        vector<int> t(n + 1);
+        t[0] = 0;
+        t[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            t[i] = t[i - 1] + t[i - 2];
+        }
+        return t[n];
     }
 };
