7654baa6de879250e6a9d3d0f62a59a77b600a65
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2022-11-01 14:23:36 +0530

    Time: 4 ms (91.68%), Space: 8.2 MB (7.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -11,40 +11,20 @@ public:
         return true;
     }
     bool isPalindrome(string s) {
-//         stringstream ss(s);
-//         string word;
-//         vector<string> v;
-//         while(ss >> word) {
-//             if(word[word.length() - 1] > 122 || word[word.length() - 1] < 65)
-//                 word.pop_back();
-//             if(word[0] >= 48 && word[0] <= 57)
-//                 continue;
-//             if(word[0] > 122 || word[0] < 65 && word.size() >= 2) {
-//                 for(int i = 0; i < word.size() - 1; i++) 
-//                     word[i] += word[i + 1];
-//                 word.pop_back();
-//             }
-//             v.push_back(word);  
-//         }
-//         string text = "";
-//         for(auto x: v)
-//             text += x;
-//         for(int i = 0; i < text.length(); i++) {
-//             if(text[i] >= 48 && text[i] <= 57)
-//                 continue;
-//             if(text[i] < 97)
-//                 text[i] = text[i] + 32;
-//         }
-        
-//         if(XisPalindrome(text))
-//             return true;
-//         return false;
-        string n;
-        for(int i = 0; i < s.length(); i++) {
-            if(isalnum(s[i])) 
-                n.push_back(tolower(s[i]));
+        stringstream ss(s);
+        string word;
+        string ans;
+        while(ss >> word) {
+            for(char x: word) {
+                if(isdigit(x))
+                    ans += x;
+                else if(int(x) >= 97 and int(x) <= 122)
+                    ans += x;
+                else if(int(x) <= 90 and int(x) >= 65)
+                    ans += char(int(x) + 32);
+            }
         }
-        if(XisPalindrome(n))
+        if(XisPalindrome(ans))
             return true;
         return false;
     }
